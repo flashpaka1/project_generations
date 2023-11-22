9fc92d99557e2b9f6f78fe1510cb6719dd9d8113
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2023-11-21 20:33:43 -0600

    starded the movement plane

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import tkinter as tk
-import Grid as Grid
+import pygame
+import sys
 
-def show_box():
-    grid_instance.Grid().printStr()
-    canvas.create_rectangle(50, 50, 200, 200, fill="blue")
+background_colour = (255, 255, 255)
+screen_h, screen_w = 1000, 1000
+screen = pygame.display.set_mode((screen_h, screen_w))
+pygame.display.set_caption('Darwin')
 
-window  = tk.Tk()
-window.title('Box Demo')
+object_color = (0, 0, 225)
+object_w, object_h = 10, 10
+object_x, object_y = (screen_w - object_w) // 2, (screen_h - object_h) // 2
+object_speed = 1
 
-canvas = tk.Canvas(window, width = 500, height = 500)
-canvas.pack()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-grid_instance = Grid()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        object_x -= object_speed
+    if keys[pygame.K_RIGHT]:
+        object_x += object_speed
+    if keys[pygame.K_UP]:
+        object_y -= object_speed
+    if keys[pygame.K_DOWN]:
+        object_y += object_speed
 
-show_box()
+    screen.fill(background_colour)
+    pygame.draw.rect(screen, object_color, (object_x, object_y, object_w, object_h))
+    pygame.display.flip()
 
-window.mainloop()
+pygame.quit()
+sys.exit()
